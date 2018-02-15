f0a48cce2a35ce66b4dda90929222ea29ed13399
Guillermo Figueroa Mesa <gfirem@gmail.com>
2018-02-15 17:00:39 -0300

    Adding data in the same row of the intent

--- content.js
+++ content.js
@@ -1,57 +1,51 @@
 "use strict";
-const API_KEY = '991536d61ae34675bf2bf9a05c8d1497';
+const API_KEY = 'e53a5f857f614013b92f07c2928018d2';
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    function(request, sender, sendResponse) {
         if (request.message === "clicked_browser_action") {
-            jQuery('#test-console').prepend('<div id="dialogflow-steroids">' +
-                '<p><strong>Filter by Event</strong></p>' +
-                '<input type="text" value="" id="dialogflow-steroids-filter-context" class="dialogflow-steroids"/>' +
-                '<div id="dialogflow-steroids-content"></div>' +
-                '</div>');
-            jQuery('#dialogflow-steroids').height('130px');
-            var content = jQuery('#dialogflow-steroids-content');
             dialogFlowSteroids().init();
         }
     }
 );
 
 function dialogFlowSteroids() {
-    var intents = {};
-
-    function filterContext() {
-        var filterBy = jQuery(this).val();
-        jQuery.each(intents, function (index, item) {
-            if (item.events && item.events.length > 0) {
-                jQuery.each(item.events, function (index, event) {
-                    if (filterBy == event.name) {
-                        console.log(item);
-                    }
-                });
-            }
-        });
-    }
-
     function getIntentList() {
         jQuery.ajax
         ({
             type: "GET",
             url: "https://api.dialogflow.com/v1/intents?v=20150910",
             dataType: 'json',
-            success: function (response) {
-                intents = response;
+            success: function(response) {
+                if (response) {
+                    jQuery.each(jQuery('span.link-intent-edit>span.ng-binding'), function(i, e) {
+                        jQuery.each(response, function(intent_index, intent_item) {
+                            if (intent_item['name'].toLocaleLowerCase() === jQuery(e).text().toLocaleLowerCase()) {
+                                var output = '<b>Action:</b>&nbsp;' + intent_item['actions'].join(', ');
+                                if (intent_item['contextIn'].length > 0) {
+                                    output += '&nbsp;&nbsp;<b>ContextIn:</b>&nbsp;' + intent_item['contextIn'].join(', ');
+                                }
+                                if (intent_item['contextOut'].length > 0) {
+                                    output += '&nbsp;&nbsp;<b>ContextOut:</b>&nbsp;' + intent_item['contextOut'].join(', ');
+                                }
+                                if (intent_item['events'].length > 0) {
+                                    output += '&nbsp;&nbsp;<b>Event:</b>&nbsp;' + intent_item['events'].map(event=> event.name ).join(', ');
+                                }
+                                jQuery(e).append('<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span> ' + output + '</span>');
+                            }
+                        });
+                    });
+                }
             },
-            beforeSend: function (xhr) {
+            beforeSend: function(xhr) {
                 xhr.setRequestHeader("Authorization", "Bearer " + API_KEY);
             }
         });
     }
 
     return {
-        init: function () {
-            if (document.getElementById('dialogflow-steroids') !== null) {
+        init: function() {
+            if (jQuery('ul.list-group') && jQuery('ul.list-group').find('li').length > 0) {
                 getIntentList();
-                var $newFields = jQuery('#dialogflow-steroids-filter-context');
-                $newFields.on('change', filterContext);
             }
         }
     }
